c656398a0febfaeeef83d4bb039f5be9570f546a
Franck W <franck.rj.w@gmail.com>
2019-04-18 14:18:37 +0200

    Ajout du passage des printf par parametre.

--- speed_test.sh
+++ speed_test.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
-stdTestFile="/tmp/std_printf_speed_test_tmp_file.txt"
-ftTestFile="/tmp/ft_printf_speed_test_tmp_file.txt"
+baseTestFile="/tmp/printf_speed_test_tmp_file.txt."
+printfExecLst=()
+
 jobsId=()
 trap 'if [[ ! -z "${jobsId[@]}" ]]; then kill ${jobsId[@]} &>/dev/null; fi; rm -rf "$stdTestFile" "$ftTestFile"; exit 130' EXIT HUP TERM INT
 
-echo "Test de la vitesse du std printf."
-rm -rf "$stdTestFile"
-./std_printf_speed > "$stdTestFile" & jobsId+=($!); sleep 10; kill -9 $!; wait $! 2>/dev/null
+for param in "$@"; do
+	nbOfColonInParam="$(echo -n "$param" | tr -dc ':' | wc -c | tr -d $' \r\n\t')"
+	if [[ "$nbOfColonInParam" == "0" ]]; then
+		printfExecLst+=("$param")
+	else
+		echo "Erreur, parametre « $param » invalide."
+		exit 0
+	fi
+done
 
-echo "Test de la vitesse du ft printf."
-rm -rf "$ftTestFile"
-./ft_printf_speed > "$ftTestFile" & jobsId+=($!); sleep 10; kill -9 $!; wait $! 2>/dev/null
+nbOfExec="${#printfExecLst[@]}"
+nameMaxSize="0"
 
-echo "Resultat du std printf : $(cat "$stdTestFile" | wc -l | tr -d ' \r\n\t')"
-echo "Resultat du ft printf  : $(cat "$ftTestFile" | wc -l | tr -d ' \r\n\t')"
-rm -rf "$stdTestFile"
-rm -rf "$ftTestFile"
+if [[ "$nbOfExec" == "0" ]]; then
+	echo "Erreur, aucun printf a tester."
+	exit 0
+fi
+
+idx="0"
+while [[ "$idx" != "$nbOfExec" ]]; do
+	param="${printfExecLst[$idx]}"
+
+	if [[ "${#param}" -gt "$nameMaxSize" ]]; then
+		nameMaxSize="${#param}"
+	fi
+	echo "Test de la vitesse de ${param}."
+	rm -rf "${baseTestFile}${idx}"
+	"${param}" > "${baseTestFile}${idx}" & jobsId+=($!); sleep 10; kill -9 $!; wait $! 2>/dev/null
+
+	(( ++idx ))
+done
+
+idx="0"
+while [[ "$idx" != "$nbOfExec" ]]; do
+	param="${printfExecLst[$idx]}"
+
+	printf 'Resultat de %-*s : ' "$nameMaxSize" "$param"
+	echo "$(cat "${baseTestFile}${idx}" | wc -l | tr -d $' \r\n\t')"
+	rm -rf "${baseTestFile}${idx}"
+
+	(( ++idx ))
+done
